63fc55664b9bbb55260a538ee87589c1c0608dda
lutenatsele <67562065+lutenatsele@users.noreply.github.com>
2024-12-08 10:22:32 +0300

    First Commit

---
+++ condition.java
@@ -0,0 +1,16 @@
+
+public class condition {
+    public static void main(String[] args) {
+         int x = 10;
+         int y = 20;
+         if (x < y) {
+             System.out.println("x is less than y");
+         } else if (x > y) {
+             System.out.println("x is greater than y");
+         } else {
+             System.out.println("x is equal to y");
+         }
+    }
+
+    }
+
